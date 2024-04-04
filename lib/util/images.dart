b059391ce6804e726cc00c913abd7a9f43c7ec12
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-04-05 02:54:05 +0530

    login ui demo

--- lib/util/images.dart
+++ lib/util/images.dart
@@ -1,19 +1,109 @@
-class Images {
-  // for Icon
-  static const String search = 'assets/icon/search.png';
-  static const String doneWithFullBackground = 'assets/icon/done_with_full_background.png';
-  static const String location = 'assets/icon/location.png';
+import 'package:flutter/material.dart';
 
-  // for Image
-  static const String arabic = 'assets/image/arabic.png';
-  static const String unitedKindom = 'assets/image/united_kindom.png';
-  static const String done = 'assets/icon/done.png';
-  static const String logOut = 'assets/image/log_out.png';
-  static const String money = 'assets/image/money.png';
-  static const String guestLogin = 'assets/image/guest_login.png';
-  static const String login = 'assets/image/login.png';
-  static const String placeholder = 'assets/image/placeholder.jpg';
-  static const String logo = 'assets/image/logo.png';
-  static const String splashLogo = 'assets/image/logo_splash.png';
-  static const String notificationIcon = 'assets/image/logo_splash.png';
+/// This class contain the all images in the string formats.
+class TImages {
+
+  // -- App Logos
+  static const String darkAppLogo = "assets/logos/logo.jpg";
+  static const String lightAppLogo = "assets/logos/logo.jpg";
+
+  // -- Social Logos
+  static const String google = "assets/images/login_screen_images/google_icon.jpeg";
+  static const String facebook = "assets/images/login_screen_images/facebook_icon.png";
+
+  // onBoarding Images
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/i.png";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/ii.png";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/iii.png";
+
+  // email verification Logo
+  static const String sendEmail = "assets/images/email_verification/email_send.png";
+  static const String sendEmailSuccessful = "assets/images/email_verification/email_send_successfully.jpeg";
+
+  //-- User Profile temp
+  static const String userProfileTemp = "assets/images/profile/user_temp.png";
+
+  // Lottie animation
+  static const String emailSendSuccessfullyAnimation = "assets/animations/120978-payment-successful.json";
+  static const String loadingAnimation = "assets/animations/141594-animation-of-docer.json";
+
+
+  // --Animation
+  static const String productIllustration = "";
+  static const String productSaleIllustration = "";
+  static const String staticSuccessIllustration = "";
+  static const String deliveredInPlaneIllustration = "";
+  static const String verifyIllustration = "";
+
+  //-- Categories Icons
+  static const String sportsIcon = "";
+  static const String clothIcon = "assets/images/store_icons/cloth_icon.png";
+  static const String shoeIcon = "";
+  static const String cosmeticsIcon = "";
+  static const String animalIcon = "";
+  static const String toyIcon = "";
+  static const String furnitureIcon = "";
+  static const String jeweleryIcon = "";
+  static const String electronicIcon = "";
+
+  //-- Payment Method
+  static const String paypalMethod = "assets/images/payment_method/paypal.png";
+  static const String successPayment = "assets/images/payment_method/success_payment.gif";
+  static const String mastercard = "assets/TAllimage/icons/payment_methods/master-card.png";
+  static const String googlePay = "assets/TAllimage/icons/payment_methods/google-pay.png";
+  static const String creditCard = "assets/TAllimage/icons/payment_methods/credit-card.png";
+  static const String paytm = "assets/TAllimage/icons/payment_methods/paytm.png";
+
+
+
+  //--Nike Logo original
+  static const String nikeLogoOrg = "assets/images/products/nike_orginal_logo.png";
+
+  //-- Reviews image logo
+  static const String userProfileReview1 = "assets/images/rating_reviews/ankit_fluom_profile.png";
+
+  // --Banner
+  static const String promoBanner1 = "assets/images/banners/sport-shoes-sale-banner.png";
+  static const String promoBanner2 = "assets/images/banners/nike-shoes-facebook.jpg";
+  static const String promoBanner3 = "assets/images/banners/sneakers_images_banners.jpg";
+
+  // Products
+  static const String productImage1 = "assets/images/products/nike_shoes2-removebg.png";
+  static const String productImageNikeJacket = "assets/images/store_icons/nike_jacket.png";
+
+  //--Home VerticalSlide Icons
+  static const String SliderchappelIcons = "assets/TAllimage/icons/categories/icons8-shoes-64.png";
+  static const String SliderschoolUniform = "assets/TAllimage/icons/categories/icons8-school-uniform-64.png";
+  static const String SlidersportsBowing = "assets/TAllimage/icons/categories/icons8-bowling-64.png";
+  static const String Slidercosmetics = "assets/TAllimage/icons/categories/icons8-cosmetics-64.png";
+  static const String SliderFurnitureDiningChair = "assets/TAllimage/icons/categories/icons8-dining-chair-64.png";
+  static const String SliderdogGHeart = "assets/TAllimage/icons/categories/icons8-dog-heart-64.png";
+  static const String Slidersmartphone = "assets/TAllimage/icons/categories/icons8-smartphone-64.png";
+  static const String SliderSparklingDiamond = "assets/TAllimage/icons/categories/icons8-sparkling-diamond-64.png";
+  static const String Slidertailor = "assets/TAllimage/icons/categories/icons8-tailors-dummy-64.png";
+  static const String SliderToyCar = "assets/TAllimage/icons/categories/icons8-wooden-toy-car-50.png";
+
+// -- Sub Categories
+  static const String SportsShoes = "assets/TAllimage/products/nike-shoes.png";
+  static const String TrackSuits = "assets/TAllimage/products/tracksuit_black.png";
+  static const String yellowTshirt = "assets/TAllimage/products/tshirt_yellow_collar.png";
+
+// --Furniture
+  static const String BedroomFurniture = "assets/TAllimage/products/bedroom_bed.png";
+  static const String KitchenDiningTable = "assets/TAllimage/products/kitchen_dining table.png";
+  static const String OfficeTableChair = "assets/TAllimage/products/office_desk_1.png";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- lib/util/strings.dart
+++ lib/util/strings.dart
@@ -1,26 +1,71 @@
-class Strings {
-  static const String chooseTheLanguage = 'Choose the language';
-  static const String findLanguage = 'Find Language';
-  static const String english = 'English';
-  static const String germany = 'Germany';
-  static const String korean = 'Korean';
-  static const String italian = 'Italian';
-  static const String chinese = 'Chinese';
-  static const String japanese = 'Japanese';
-  static const String arabic = 'Arabic';
-  static const String save = 'Save';
-  static const String skip = 'Skip';
-  static const String next = 'Next';
-  static const String previous = 'Previous';
-  static const String letsStart = 'Let\'s Start';
-  static const String welcome = 'Welcome';
-  static const String loremIpsum = 'Lorem ipsum dolor sit amet, consetetur the sadipscing elitr, sed diam nonu.';
-  static const String login = 'Login';
-  static const String signUp = 'Singup';
-  static const String mobileNumber = 'Mobile Number';
-  static const String enterMobileNumber = 'Enter Mobile Number';
-  static const String password = 'Password';
-  static const String rememberMe = 'Remember me';
-  static const String forgotPassword = 'Forgot passoword ?';
-  static const String createAnAccount = 'Create an Account ?';
+import 'package:flutter/material.dart';
+
+/// This calss contain all the App Text in String formats.
+class TTexts {
+
+  // -- Global Texts
+  static const String and = "and";
+  static const String continues = "Continue";
+  static const String submit = "Submit";
+  static const String done = "Done";
+  static const String skip = "Skip";
+  static const String dones = "Done";
+  static const String appName = "Fluom";
+
+
+  // -- OoBoarding Texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choice - Your Perfect Product Awaits!";
+  static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubTitle3 = "From Our Doorstep to Your - Swift, Secure and Contactless Delivery!";
+
+
+  // Authentication Form Text
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String userName = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn = "Sign IN";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of use";
+  static const String verificationCode = "VerificationCode";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+
+  // Authentication Heading Texts
+  static const String loginTitle = "Welcome back,";
+  static const String loginSubTitle = "Discover Limitless Choices and Unmatched Convenience.";
+  static const String signUpTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forget Password";
+  static const String forgetPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password resent link.";
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+  static const String changeYourPasswordSubTitle = "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle = "Congratulations! Your Account Awaits:Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
+  static const String emailNotReceivedMessage = "Didn't get the email? Check your junk/spam or resend it.";
+  static const String yourAccountCreatedTitle = "Your account successfully created";
+  static const String yourAccountCreatedSubTitle = "Welcome to Your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping!";
+
+// --Home
+  static const String homeAppbarTitle = "Good day for shopping";
+  static const String homeAppbarSubTitle = "Ankit Dhattarwal";
+
+
+
+
+
+
+
 }
